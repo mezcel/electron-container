5742141cf4d3ad7aa432e15cff85d0f246a7a674
mezcel <mezcel@hotmail.com>
2018-09-26 11:15:18 -0400

    added files

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # electron-container
 
-Playing with Node
 
-Messing with ElectronJs and Socket.io.
+Playing with NodeJS
 
-No Mongo
+Messing with ElectronJs and Socket.io. No Mongo.
 
 This app does nothing.
 
-I just wanted to have a skeleton to pick at.
+It was just to have a skeleton to pick at.
+
+```sh
+npm install
+npm start
+```
---
+++ main.js
@@ -0,0 +1,90 @@
+
+var appchatserver = require('express')(); // chat room gui
+var http = require('http').Server(appchatserver);
+var io = require('socket.io')(http);
+
+appchatserver.get('/', function(req, res) {
+   res.sendfile('index-chatroom.html');
+});
+
+users = [];
+sessionChatLog = [];
+
+io.on('connection', function(socket) {
+   console.log('A user connected');
+   socket.on('setUsername', function(data) {
+      console.log(data);
+
+      if(users.indexOf(data) > -1) {
+         socket.emit('userExists', data + ' username is taken! Try some other username.');
+      } else {
+         users.push(data);
+         socket.emit('userSet', {username: data});
+         
+      }
+   });
+
+   socket.on('msg', function(data) {
+      //Send message to everyone
+      io.sockets.emit('newmsg', data);
+      // debug watch
+      sessionChatLog.push(data);
+      console.log(sessionChatLog);
+   })
+});
+
+http.listen(3000, function() {
+   console.log('listening on localhost:3000');
+});
+
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron') // Electron specific vars
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 700, height: 900})
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+  //mainWindow.loadURL(`http://localhost:3000/`)
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
