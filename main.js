aa2e4c91d2bfd99bae2659f51f8f0504b2be62bf
mezcel <mezcel@hotmail.com>
2018-09-27 16:29:21 -0400

    beautify script and messed with user msg colors

--- main.js
+++ main.js
@@ -1,67 +1,87 @@
-
+/* --------------------------------------------------------------------------------------------------*/
+/* Express Socket IO */
 var appmsgserver = require('express')(); // msg room gui
 var http = require('http').Server(appmsgserver);
 var io = require('socket.io')(http);
 
+function getRandomRolor() {
+    var letters = '012345'.split('');
+    var color = '#';
+    color += letters[Math.round(Math.random() * 5)];
+    letters = '0123456789ABCDEF'.split('');
+    for (var i = 0; i < 5; i++) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
+
 appmsgserver.get('/', function(req, res) {
-   res.sendfile('index-msg.html');
+    res.sendfile('index-msg.html');
 });
 
 users = [];
-sessionChatLog = [];
-
 io.on('connection', function(socket) {
-   console.log('A user connected');
-   socket.on('setUsername', function(data) {
-      console.log(data);
+    console.log('A user connected');
 
-      if(users.indexOf(data) > -1) {
-         socket.emit('userExists', data + ' username is taken! Try some other username.');
-      } else {
-         users.push(data);
-         socket.emit('userSet', {username: data});
-         
-      }
-   });
+    socket.on('setUsername', function(data) {
+        // var randomHexVal = (Math.random()*0xFFFFFF<<0).toString(16);
+        var randomHexVal = getRandomRolor();
 
-   socket.on('msg', function(data) {
-      //Send message to everyone
-      io.sockets.emit('newmsg', data);
-      // debug watch
-      sessionChatLog.push(data);
-      console.log(sessionChatLog);
-   })
+        if (users.indexOf(data) > -1) {
+            socket.emit('userExists', data + ' username is taken! Try some other username.');
+        } else {
+
+            users.push(data);
+            socket.emit('userSet', {
+                username: data,
+                colorname: randomHexVal
+            });
+        }
+    });
+
+    socket.on('msg', function(data) {
+        //Send message to everyone
+        io.sockets.emit('newmsg', data);
+    })
 });
 
 http.listen(3000, function() {
-   console.log('listening on localhost:3000');
+    console.log('listening on localhost:3000');
 });
 
+/* --------------------------------------------------------------------------------------------------*/
+/* ELECTRON */
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron') // Electron specific vars
+const {
+    app,
+    BrowserWindow
+} = require('electron') // Electron specific vars
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 700, height: 900})
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 700,
+        height: 900
+    })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-  //mainWindow.loadURL(`http://localhost:3000/`)
+    // and load the index.html of the app.
+    mainWindow.loadFile('index.html')
+    //mainWindow.loadURL(`http://localhost:3000/`)
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
 }
 
 // This method will be called when Electron has finished
@@ -70,20 +90,20 @@ function createWindow () {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on('activate', function() {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
