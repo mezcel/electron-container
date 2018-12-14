ac2d5a016b78c41fb832ec92f536c7df0573256f
mezcel <mezcel@hotmail.com>
2018-12-13 20:36:10 -0500

    tweaked comments

--- install-electron-archlinux.sh
+++ install-electron-archlinux.sh
@@ -3,6 +3,9 @@
 ## At the time of making this app, there 'were' issues with just blindly installing Electron as a dev dependancy.
 ## This script is recommended but not required. Install this script first as not-root. Installing as root 'might' give Nodejs mixed messages about your intended usecase and user permissions.
 
+currentDirPath=$(dirname $0)
+cd $currentDirPath
+
 sudo pacman -S --needed nodejs
 sudo pacman -S --needed npm
 
