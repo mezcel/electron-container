683814fbe036938c905459ff20526b05e47c4bb2
mezcel <mezcel@hotmail.com>
2018-12-23 00:08:30 -0500

    ubuntu node installer

---
+++ install-electron-ubuntu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Node.js v10.x:
+## Using Ubuntu
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+## Using Debian, as root
+# curl -sL https://deb.nodesource.com/setup_10.x | bash -
+# apt-get install -y nodejs
+
+## Node.js v8.x:
+## Using Ubuntu
+# curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+# sudo apt-get install -y nodejs
+
+## Using Debian, as root
+# curl -sL https://deb.nodesource.com/setup_8.x | bash -
+# apt-get install -y nodejs
