8e52551290fdc2a5e6478383aebbb7a880c4664d
mezcel <mezcel@hotmail.com>
2019-01-28 19:28:27 -0500

    renamed file

---
+++ install-electron-debian.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## Node.js v10.x:
+## Using Ubuntu
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+## Using Debian, as root
+# curl -sL https://deb.nodesource.com/setup_10.x | bash -
+# apt-get install -y nodejs
+
+## Node.js v8.x:
+## Using Ubuntu
+# curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+# sudo apt-get install -y nodejs
+
+## Using Debian, as root
+# curl -sL https://deb.nodesource.com/setup_8.x | bash -
+# apt-get install -y nodejs
+
+## App Npm's
+
+npm install express
+npm install socket.io
+npm install poppler.js
+npm install electron
+
+npm install
+
