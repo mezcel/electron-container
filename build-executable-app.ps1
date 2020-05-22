67557cbae6a65b014780dfe0ac6ab57971864cfd
mezcel <mezcel@hotmail.com>
2020-05-22 09:40:41 -0400

    added an exe packager

---
+++ build-executable-app.ps1
@@ -0,0 +1,77 @@
+<#
+    build-executable-app.ps1
+    remove previous build and package a new build
+#>
+
+function killRunningProcess() {
+    ## kill any running  processes
+    Write-Host "kill any running electron-rosary* processes ..." -ForegroundColor Cyan
+    Stop-Process -Name "electron-rosary*"
+    Start-Sleep 3
+}
+
+function removePreviousBuild() {
+    ## rm old build
+    Write-Host "Remove previous build ..." -ForegroundColor Cyan
+    #Remove-Item -Recurse -Force ".\electron-rosary-win32-x64" -ErrorAction Ignore
+    #Remove-Item -Recurse -Force ".\electron-rosary-win32-ia32" -ErrorAction Ignore
+    Remove-Item -Recurse -Force ".\electron-rosary-win32-*" -ErrorAction Ignore
+    Start-Sleep 3
+
+}
+
+function buildNewPackage() {
+    ## build new package
+    Write-Host "Build a new package ..." -ForegroundColor Cyan
+    electron-packager .
+    Start-Sleep 3
+}
+
+function set-shortcut( [string]$ShortcutFile, [string]$WorkingDir, [string]$TargetFile ) {
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+    $Shortcut.TargetPath = $TargetFile
+    $Shortcut.WorkingDirectory = $WorkingDir
+    $Shortcut.Save()
+}
+function createShortcutLinks() {
+    ## Make a shortcut link
+    Write-Host "Creating shortcut links..." -ForegroundColor Cyan
+    Set-Location -Path electron-rosary*
+
+    $verbosePath = Get-Location
+    $appName = "electron-rosary"
+    $linkName = "$appName.lnk"
+    $exeName = "$appName.exe"
+    $ShortcutFile1 = "$env:UserProfile\Desktop\$linkName"
+    #$ShortcutFile2 = "$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\$linkName"
+
+    $WorkingDir = "$verbosePath"
+    $TargetFile = "$verbosePath\$exeName"
+
+    ## desktop
+    set-shortcut $ShortcutFile1 $WorkingDir $TargetFile
+    Write-Host "Created a desktop shortcut ..." -ForegroundColor Cyan
+    ## startup
+    #set-shortcut $ShortcutFile2 $WorkingDir $TargetFile
+    #Write-Host "Created a startup link ..." -ForegroundColor Cyan
+
+    Set-Location -Path ..\
+
+}
+
+function main() {
+    ## Install npm package
+    #npm install -g electron-packager
+
+    killRunningProcess
+    removePreviousBuild
+    buildNewPackage
+    createShortcutLinks
+}
+
+<# RUN #>
+
+main
+
+Write-Host "Done.`n"-ForegroundColor Yellow
